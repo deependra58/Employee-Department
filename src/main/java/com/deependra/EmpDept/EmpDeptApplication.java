2b2964be8cc4d7e6991d7cdffbf30f0b770cec43
deependra58 <karkideependra58@gmail.com>
2017-09-17 20:32:46 +0545

    complete department and employee only

---
+++ src/main/java/com/deependra/EmpDept/EmpDeptApplication.java
@@ -0,0 +1,12 @@
+package com.deependra.EmpDept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDeptApplication.class, args);
+	}
+}
